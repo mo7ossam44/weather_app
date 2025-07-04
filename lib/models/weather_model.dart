df9573ec9db6f5fcca5e6796c63f350ce7c75294
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-05 01:58:48 +0300

    change data types and other things in factory model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,31 +1,31 @@
 class WeatherModel {
   final String name;
+  final double tempC;
+  final String? icon;
+  final double maxtempC;
+  final double mintempC;
   final String lastUpdated;
-  final String tempC;
-  final String icon;
-  final String maxtempC;
-  final String mintempC;
   final String weatherStatus;
 
   WeatherModel({
+    this.icon,
     required this.name,
-    required this.lastUpdated,
     required this.tempC,
-    required this.icon,
-    required this.maxtempC,
     required this.mintempC,
+    required this.maxtempC,
+    required this.lastUpdated,
     required this.weatherStatus,
   });
 
   factory WeatherModel.fromJson(Map<String, dynamic> json) {
     return WeatherModel(
-      name: json['location']['name'],
-      lastUpdated: json['current']['last_updated'],
-      tempC: json['current']['temp_c'],
-      icon: json['current']['condition']['icon'],
-      maxtempC: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
-      mintempC: json['forecast']['forecastday'][0]['day']['mintemp_c'],
-      weatherStatus: json['current']['condition']['text'],
+      name: json['location']['name'] ?? '',
+      lastUpdated: json['current']['last_updated'] ?? '',
+      tempC: json['current']['temp_c'] ?? 0,
+      icon: json['current']['condition']['icon'] ?? '',
+      maxtempC: json['forecast']['forecastday'][0]['day']['maxtemp_c'] ?? 0,
+      mintempC: json['forecast']['forecastday'][0]['day']['mintemp_c'] ?? 0,
+      weatherStatus: json['current']['condition']['text'] ?? '',
     );
   }
 }
